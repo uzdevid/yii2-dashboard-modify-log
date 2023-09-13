244ef129471da28e88a7dcf77d218c49539755d5
uzdevid <uzdevid@gmail.com>
2023-09-13 17:48:41 +0500

    Добавил бутстрап для модуля

---
+++ bootstraps/ModifyLog.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace uzdevid\dashboard\access\control\bootstraps;
+
+use uzdevid\dashboard\events\ModuleEvent;
+use uzdevid\dashboard\modify\log\controllers\ModifyLogController;
+use uzdevid\dashboard\modules\system\Module;
+use yii\base\BootstrapInterface;
+
+class ModifyLog implements BootstrapInterface {
+    public function bootstrap($app) {
+        $app->on(Module::EVENT_AFTER_INIT, function (ModuleEvent $event) {
+            $event->module->controllerMap['modify-log'] = ModifyLogController::class;
+        });
+    }
+}
